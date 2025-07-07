ef22720a4249b9594a868c6e979efc92b35762b8
Robert Crandall <robert-crandall@github.com>
2025-07-07 14:42:33 -0700

    Clean start

--- src/server/api/root.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from '@/server/api/trpc';
-import { authRouter } from './routers/auth';
-
-export const appRouter = createTRPCRouter({
-  auth: authRouter,
-});
-
-export type AppRouter = typeof appRouter;
