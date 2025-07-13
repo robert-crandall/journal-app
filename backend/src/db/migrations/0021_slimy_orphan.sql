fac93d88518afa226610d63cc6f3a6d0494d17fb
Robert Crandall <robert-crandall@github.com>
2025-07-13 16:37:39 -0700

    Remove tag fields

---
+++ backend/src/db/migrations/0021_slimy_orphan.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "journals" DROP COLUMN "tone_tags";--> statement-breakpoint
+ALTER TABLE "journals" DROP COLUMN "content_tags";--> statement-breakpoint
+ALTER TABLE "journals" DROP COLUMN "stat_tags";
