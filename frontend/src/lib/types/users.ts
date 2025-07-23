4140b5bf05270b906cf1ee4f9c04770c72fde317
Robert Crandall <robert-crandall@github.com>
2025-07-23 10:29:31 -0700

    Extract types

---
+++ frontend/src/lib/types/users.ts
@@ -0,0 +1,2 @@
+// Barrel file for user types, re-exporting from backend
+export type { User, NewUser, UserUpdate, PublicUser, UpdateUserRequest, UpdateUserAvatarRequest } from '../../../../backend/src/types/users';
