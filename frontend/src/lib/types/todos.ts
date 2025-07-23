881c6afdc847e2fa11fbf9a6a396d7c8de1bb129
Robert Crandall <robert-crandall@github.com>
2025-07-23 10:06:39 -0700

    Extract todos

---
+++ frontend/src/lib/types/todos.ts
@@ -0,0 +1,9 @@
+// Barrel file for simple todo types, re-exporting from backend
+export type {
+  SimpleTodo,
+  NewSimpleTodo,
+  UpdateSimpleTodo,
+  CreateSimpleTodoRequest,
+  UpdateSimpleTodoRequest,
+  SimpleTodoResponse,
+} from '../../../../backend/src/types/todos';
