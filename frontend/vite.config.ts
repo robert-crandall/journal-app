147b2bd88eebceb1bbfc0978f144fc3d22b32549
Robert Crandall <robert-crandall@github.com>
2025-08-28 21:55:20 -0700

    Remove vite tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,32 +5,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-  test: {
-    projects: [
-      {
-        extends: './vite.config.ts',
-        test: {
-          name: 'client',
-          environment: 'browser',
-          browser: {
-            enabled: true,
-            provider: 'playwright',
-            instances: [{ browser: 'chromium' }],
-          },
-          include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-          exclude: ['src/lib/server/**'],
-          setupFiles: ['./vitest-setup-client.ts'],
-        },
-      },
-      {
-        extends: './vite.config.ts',
-        test: {
-          name: 'server',
-          environment: 'node',
-          include: ['src/**/*.{test,spec}.{js,ts}'],
-          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-        },
-      },
-    ],
-  },
 });
